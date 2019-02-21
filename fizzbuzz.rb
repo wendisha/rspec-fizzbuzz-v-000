4ae5dd36112b81e3b8bf2809342df05152b88f96
Wendy Calderon <wendycalderon@gmail.com>
2019-02-21 04:00:51 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,9 +1,9 @@
 def fizzbuzz(int)
-  if int % 3 == 0 && int % 5 == 0
-    "FizzBuzz"
-  elsif int % 5 == 0
-    "Buzz"
-  elsif int % 3 == 0
+if int % 5 == 0 && int % 3 == 0
+  "FizzBuzz"
+elsif int % 3 == 0
     "Fizz"
+  else int % 5 == 0
+    "Buzz"
   end
 end
